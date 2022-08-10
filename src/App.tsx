bc58c5d5585e9890ef56081abb91a01f39fc95ec
OperatorJack <thepaladinof@gmail.com>
2022-08-09 22:57:52 -0400

    Initial app scaffold + Drawer implementation.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Container />
+    </>
+  );
+}
+
+export default App;
