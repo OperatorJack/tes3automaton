b62f77ff621dd7e8c4fa612f9de19149a589b080
OperatorJack <thepaladinof@gmail.com>
2022-08-10 23:21:54 -0400

    Store files in state and list on screen in simple list.

--- src/components/views/settings/SelectDirectoryButton.tsx
+++ src/components/views/settings/SelectDirectoryButton.tsx
@@ -3,32 +3,47 @@ import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 
 import { invoke } from "@tauri-apps/api/tauri";
 import { open } from "@tauri-apps/api/dialog";
-
-async function collectFiles() {
-    const selected = await open({ directory: true, multiple: false });
-    if (typeof selected != "string") {
-        return;
-    }
-
-    invoke("collect_files", {
-        path: selected,
-        extensions: ["nif"],
-    })
-        .then((files) => console.log(files))
-        .catch((e) => console.error(e));
-}
+import React from "react";
 
 export function SelectDirectoryButton() {
+    const [files, setFiles] = React.useState([] as string[]);
+    const collectFiles = async function () {
+        const selected = await open({ directory: true, multiple: false });
+        if (typeof selected != "string") {
+            return;
+        }
+
+        try {
+            const files = await invoke("collect_files", {
+                path: selected,
+                extensions: ["nif"],
+            });
+
+            console.log(files);
+            setFiles(files as string[]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <FormGroup>
-            <FormControlLabel
-                control={
-                    <IconButton color="inherit" onClick={collectFiles}>
-                        {<FolderOpenIcon />}
-                    </IconButton>
-                }
-                label={"Select Folder"}
-            />
-        </FormGroup>
+        <>
+            <FormGroup>
+                <FormControlLabel
+                    control={
+                        <IconButton color="inherit" onClick={collectFiles}>
+                            {<FolderOpenIcon />}
+                        </IconButton>
+                    }
+                    label={"Select Folder"}
+                />
+            </FormGroup>
+            <div>
+                {files.length > 0 &&
+                    files.map((file: string) => {
+                        return <div key={file}>{file}</div>;
+                    })}
+            </div>
+        </>
     );
 }
